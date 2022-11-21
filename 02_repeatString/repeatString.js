a7d064b784070a60c6d72df5e9e7925a85030d3e
zoink506 <benhubble03@gmail.com>
2022-11-21 23:26:34 +1100

    Fix 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let completeStr = "";
 
+  if(num >= 0) {
+    for(let i = 0; i < num; i++) {
+      completeStr = completeStr + str;
+    }
+  } else {
+    completeStr = "ERROR";
+  }
+
+  return completeStr;
 };
 
 // Do not edit below this line

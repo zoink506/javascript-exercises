0818b4a1fa3eca21123671f53422507809bce82d
zoink506 <benhubble03@gmail.com>
2022-11-21 23:43:23 +1100

    Fix 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let newString = "";
+  for(let i = str.length; i >= 0; i--) {
+    newString = newString.concat(str.charAt(i));
+  }
 
+  return newString;
 };
 
 // Do not edit below this line

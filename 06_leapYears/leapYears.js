b251cac2c4305a9fa236e9f445abd2b617b476c5
zoink506 <benhubble03@gmail.com>
2022-11-22 12:16:56 +1100

    Check for leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if(typeof year === "number") {
+    if( !(year % 4) && ((year % 100 != 0) || !(year % 400)) ) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

5be5e49c00ce3c0d0ec58b7a9bdba3455a511ef6
zoink506 <benhubble03@gmail.com>
2022-11-22 11:47:49 +1100

    Fix 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if((num1 >= 0 && num2 >= 0) && (typeof num1 === "number" && typeof num2 === "number")) {
+    let sum = 0;
+    let a = 0;
+    let b = 0;
+    
+    if(num1 <= num2) {
+      a = num1;
+      b = num2;
+    } else {
+      a = num2;
+      b = num1;
+    }
 
+    while(a <= b) {
+      sum += a;
+      a++;
+    }
+
+    console.log(`Final sum: ${sum}`);
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

c3913ec3890da166328c243677d86079c181309d
zoink506 <benhubble03@gmail.com>
2022-11-22 11:12:44 +1100

    Fix 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+  let i = 1;
+  while(i < arguments.length) { // Loop through arguments
 
+    let j = 0;
+    while(j < arr.length) {
+      if(arr[j] === arguments[i]) {
+        arr.splice(j, 1);
+      } else {
+        j++;
+      }
+    }
+
+    i++;
+  }
+
+  return arr;
 };
 
 // Do not edit below this line

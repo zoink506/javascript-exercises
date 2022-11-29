dfb9fbc46bc5408029d9a87c87a971a909e58058
zoink506 <benhubble03@gmail.com>
2022-11-30 01:07:16 +1100

    Loop through array and pull book titles into another array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  let bookArray = [];
+  arr.forEach(book => {
+    bookArray.push(book.title);
+  });
+  return bookArray;
 };
 
 // Do not edit below this line

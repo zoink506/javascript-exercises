c2f2f8791177157c9ec788a4852cee019f1e1171
zoink506 <benhubble03@gmail.com>
2022-11-29 18:26:42 +1100

    Fix temperature conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = Math.round( ((temp - 32) / 1.8) * 10 ) / 10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = Math.round( ((temp *1.8) + 32) * 10 ) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line

55b3fb08b9151dca634381d43bb6bd54dabe1dcf
zoink506 <benhubble03@gmail.com>
2022-11-29 18:58:30 +1100

    Fix palindrome functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let isPalindrome = false;
+  const regex = /[!"#$%&'()/\s*+,-./:;<=>?@[\]^_`{|}~]/g;
+  let cleanStr = str.replace(regex, ""); // string no longer has any punctuation
+  cleanStr = cleanStr.toLowerCase();
 
-};
+  // Loop through 
+  let newStr = "";
+  for(let i = cleanStr.length - 1; i >= 0; i--) {
+    newStr += cleanStr.charAt(i);
+  }
+  console.log(`newStr: ${newStr}`);
+  console.log(`cleanStr: ${cleanStr}`);
+
+  if(cleanStr === newStr) isPalindrome = true;
+  else isPalindrome = false;
+  return isPalindrome;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

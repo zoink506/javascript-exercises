53c07567762d03eeae7470bc56f6959857d9a9ed
zoink506 <benhubble03@gmail.com>
2022-11-29 18:27:32 +1100

    Fix calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+	// takes two arguments, adds them
+  return value1 + value2;
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+	// takes two arguments, subtracts them
+  return value1 - value2;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+  // takes an array, adds all the values in array
+  let sum = 0;
+  values.forEach(value => {
+    sum += value;
+  });
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(values) {
+  // takes an array, multiplies all values left to right | arr[0] * arr[1] * arr[2] etc.
+  let total = values[0];
+  for(let i = 1; i < values.length; i++) {
+    total = total * values[i];
+  }
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(value, power) {
+  // takes 2 arguments, 1st argument to the power of 2nd argument
+  let total = value;
+  for(let i = 1; i < power; i++) {
+    total *= value;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	// takes 1 argument, finds factorial: factorial(4) = 4 * 3 * 2 * 1 ... 
+  let total;
+  if(value === 0) {
+    total = 1;
+  } else {
+    total = value;
+    for(let i = value-1; i > 1; i--) {
+      total *= i;
+    }
+  }
+  
+  return total;
 };
 
 // Do not edit below this line

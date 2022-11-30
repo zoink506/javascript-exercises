e4097ebcc2ae7df2d8e6536b9ef795c1df2de1de
zoink506 <benhubble03@gmail.com>
2022-11-30 20:01:17 +1100

    Add function to find oldest person in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  // Return the oldest person
+  let oldestAge = 0;
+  let oldestPerson;
+  const today = new Date();
+  const year = today.getFullYear();
 
+  arr.forEach(person => {
+    let age;
+    if(person.hasOwnProperty('yearOfDeath')) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      age = year - person.yearOfBirth;
+    }
+
+    if(age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+
+  });
+
+  console.log(oldestPerson);
+  return oldestPerson;
 };
 
 // Do not edit below this line
